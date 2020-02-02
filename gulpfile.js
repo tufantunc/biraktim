1a5d7d8a54f76f5927b5226660ea75143fe7f2e6
Tufan Tunç <19643687+tufantunc@users.noreply.github.com>
2020-02-02 22:43:47 +0300

    new website

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,20 @@ const purgecss = require('gulp-purgecss');
 const cleanCss = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const browserSync = require('browser-sync');
+const minify = require('gulp-minify');
+const clean = require('gulp-clean');
+const critical = require('critical').stream;
+const puppeteer = require("puppeteer");
+const tap = require("gulp-tap");
+const sitemap = require('gulp-sitemap');
 
 sass.compiler = require('node-sass');
 
 const server = browserSync.create();
+
+gulp.task('asset-copy', () => gulp.src('./src/assets/**')
+    .pipe(gulp.dest('./dist'))
+);
 
 gulp.task('sass', () => gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -19,7 +29,8 @@ gulp.task('sass', () => gulp.src('./src/*.scss')
     }))
     .pipe(postcss([ autoprefixer() ]))
     .pipe(purgecss({
-        content: ['./src/index.html']
+        content: ['./src/index.html'],
+        whitelist: ['svg', 'prog', 'bg']
     }))
     .pipe(gulp.dest('./dist'))
 );
@@ -28,6 +39,59 @@ gulp.task('html', () => gulp.src('./src/*.html')
     .pipe(htmlmin({
         collapseWhitespace: true,
         removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('critical', () => gulp.src('./dist/index.html')
+      .pipe(critical({
+        base: './dist/',
+        inline: true,
+        css: [
+          './dist/style.css'
+        ]
+      }))
+      .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('js', () => gulp.src('./src/*.js')
+    .pipe(minify({
+        noSource: true,
+        ext:{
+            min:'.js'
+        },
+    }))
+    .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('clean', () => gulp.src('./dist/', {read: false, allowEmpty: true})
+    .pipe(clean())
+);
+
+gulp.task('screenshot', () => gulp.src('./dist/index.html')
+    .pipe(tap(async (file) => {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 1200,
+            height: 630,
+            deviceScaleFactor: 1
+        });
+        await page.goto("file://" + file.path);
+        await page.screenshot({ path: "./dist/index_facebook_og.png" });
+        await page.setViewport({
+            width: 1200,
+            height: 675,
+            deviceScaleFactor: 1
+        });
+        await page.screenshot({ path: "./dist/index_twitter_card.png" });
+        await browser.close();
+    }))
+);
+
+gulp.task('sitemap', () => gulp.src('./dist/*.html')
+    .pipe(sitemap({
+        siteUrl: 'https://biraktim.tufantunc.com'
     }))
     .pipe(gulp.dest('./dist'))
 );
@@ -45,8 +109,8 @@ const serve = (done) => {
   done();
 }
 
-gulp.task('watch', () => gulp.watch('./src/*.{scss,html,js}', gulp.parallel(['sass', 'html'])));
+gulp.task('watch', () => gulp.watch('./src/*.{scss,html,js}', gulp.series(gulp.parallel(['sass', 'html', 'js']), gulp.parallel('critical', 'sitemap'))));
 
-gulp.task('dev', gulp.series(gulp.parallel('sass', 'html'),  serve, 'watch'));
+gulp.task('dev', gulp.series('clean', gulp.parallel('sass', 'html', 'js', 'asset-copy'), gulp.parallel('critical', 'sitemap'),  serve, 'watch'));
 
-gulp.task('default', gulp.parallel('sass', 'html'));
+gulp.task('default', gulp.series(gulp.parallel('sass', 'html', 'js', 'asset-copy'), gulp.parallel('critical', 'screenshot', 'sitemap')));
